0369e407d998fa63f2d4f3772552fc00db789ee8
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-07-15 09:48:53 +0300

    Merged 2 POST request into one because of a dependence
    Merged (and removed each one):
      "/link-owner-user-uuid"
      "/calculate-and-accrue-points"
    into:
      "/process-purchase" (same 2 parameters)

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ const SERVER_PORT = process.env.SERVER_PORT as string
 
 app.use(express.urlencoded({ extended: true }))
 
-app.post("/calculate-and-accrue-points", referral_data.calculate_and_accrue_points)
-
-app.post("/link-owner-user-uuid", referral_data.set_link_owner_uuid_by_referral_id)
+app.post("/process-purchase", referral_data.process_purchase)
 
 app.get("/user-referral-id/:username", user_data.get_referral_id_by_username)
 
--- src/referral_data.ts
+++ src/referral_data.ts
@@ -1,10 +1,71 @@
 import * as utils from "./utils"
 import { QueryResult, Request, Response } from "./utils"
 
+async function calculate_and_accrue_points(purchase_uuid: string, referral_id: string) {
+  let query: QueryResult<any>
+
+  query = await utils.fetch.purchase_data("uuid", purchase_uuid, "product_uuid")
+  const product_uuid = query.rows[0].product_uuid
+
+  query = await utils.fetch.product_data("uuid", product_uuid, "price")
+  const product_price = query.rows[0].price
+
+  const percent = 0.1
+  const points = Math.round(product_price * percent * 100) / 100
+
+  await utils.update.purchase_data("uuid", purchase_uuid, "earned_points", points)
+
+  update_reward_balance(referral_id)
+}
+
+async function update_link_owner_uuid(purchase_uuid: string, referral_id: string) {
+  let query: QueryResult<any>
+  query = await utils.fetch.user_data("referral_id", referral_id, "uuid")
+  if (query.rowCount === 0) {
+    return {
+      status: 404,
+      message: `referral_id "${referral_id}" was not found`
+    }
+  }
+  const link_owner_user_uuid = query.rows[0].uuid
+
+  query = await utils.fetch.purchase_data("uuid", purchase_uuid, "user_uuid")
+  const user_uuid = query.rows[0].user_uuid
+
+  if (link_owner_user_uuid === user_uuid) {
+    return {
+      status: 404,
+      message: "user_uuid can't be equal to link_owner_user_uuid, " +
+        "because user can't earn points from his own referral link"
+    }
+  }
+
+  await utils.update.purchase_data(
+    "uuid", purchase_uuid, "link_owner_user_uuid", link_owner_user_uuid)
+}
+
+async function update_reward_balance(referral_id: string) {
+  let query: QueryResult<any>
+
+  query = await utils.fetch.user_data("referral_id", referral_id, "uuid")
+  const link_owner_user_uuid = query.rows[0].uuid
+
+  query = await utils.fetch.purchase_data(
+    "link_owner_user_uuid", link_owner_user_uuid, "earned_points")
+  const earned_points_list = query.rows
+
+  let total_points = 0
+  for (const points of earned_points_list) {
+    total_points += points.earned_points
+  }
+
+  await utils.update.user_data(
+    "uuid", link_owner_user_uuid, "reward_balance", total_points)
+}
+
 namespace referral_data {
-  export async function set_link_owner_uuid_by_referral_id(request: Request, response: Response) {
+  export async function process_purchase(request: Request, response: Response) {
     const { purchase_uuid, referral_id } = request.body
-    let query: QueryResult<any>
 
     if (typeof purchase_uuid !== "string" ||
       typeof referral_id !== "string") {
@@ -12,72 +73,10 @@ namespace referral_data {
     }
 
     try {
-      query = await utils.fetch.user_data("referral_id", referral_id, "uuid")
-      if (query.rowCount === 0) {
-        response.status(404).end(`referral_id "${referral_id}" was not found`)
-      }
-      const link_owner_user_uuid = query.rows[0].uuid
+      const error = await update_link_owner_uuid(purchase_uuid, referral_id)
+      if (error) response.status(error.status).send(error.message)
 
-      query = await utils.fetch.purchase_data("uuid", purchase_uuid, "user_uuid")
-      const user_uuid = query.rows[0].user_uuid
-
-      if (link_owner_user_uuid === user_uuid) {
-        return response.status(400).send(
-          "user_uuid can't be equal to link_owner_user_uuid, " +
-          "because user can't earn points from his own referral link")
-      }
-
-      await utils.update.purchase_data(
-        "uuid", purchase_uuid, "link_owner_user_uuid", link_owner_user_uuid)
-    }
-    catch (error) {
-      response.status(500).end()
-      throw error
-    }
-    response.status(200).end()
-  }
-
-  async function update_reward_balance(referral_id: string) {
-    let query: QueryResult<any>
-
-    query = await utils.fetch.user_data("referral_id", referral_id, "uuid")
-    const link_owner_user_uuid = query.rows[0].uuid
-
-    query = await utils.fetch.purchase_data(
-      "link_owner_user_uuid", link_owner_user_uuid, "earned_points")
-    const earned_points_list = query.rows
-
-    let total_points = 0
-    for (const points of earned_points_list) {
-      total_points += points.earned_points
-    }
-
-    await utils.update.user_data(
-      "uuid", link_owner_user_uuid, "reward_balance", total_points)
-  }
-
-  export async function calculate_and_accrue_points(request: Request, response: Response) {
-    const { purchase_uuid, referral_id } = request.body
-    let query: QueryResult<any>
-
-    if (typeof purchase_uuid !== "string" ||
-      typeof referral_id !== "string") {
-      return response.status(400).send("purchase_uuid and referral_id is needed")
-    }
-
-    try {
-      query = await utils.fetch.purchase_data("uuid", purchase_uuid, "product_uuid")
-      const product_uuid = query.rows[0].product_uuid
-
-      query = await utils.fetch.product_data("uuid", product_uuid, "price")
-      const product_price = query.rows[0].price
-
-      const percent = 0.1
-      const points = Math.round(product_price * percent * 100) / 100
-
-      await utils.update.purchase_data("uuid", purchase_uuid, "earned_points", points)
-
-      update_reward_balance(referral_id)
+      await calculate_and_accrue_points(purchase_uuid, referral_id)
     }
     catch (error) {
       response.status(500).end()
